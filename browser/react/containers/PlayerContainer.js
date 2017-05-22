061bdcbdb8b3c2668fa8a8ffd6c3b0bf04a931c8
Jasiu Leja <jleja09@gmail.com>
2017-05-22 17:39:42 -0400

    done PlayerContainer refactor

--- browser/react/containers/PlayerContainer.js
+++ browser/react/containers/PlayerContainer.js
@@ -1,56 +1,52 @@
 import React, {Component} from 'react';
 import AUDIO from '../audio';
-import store from '../store';
+import {connect} from 'react-redux';
 import {previous, next, setProgress, toggleSong} from '../action-creators/player';
 import Player from '../components/Player';
 
+
+const mapStateToProps = (state) => {
+  return Object.assign({},
+  {
+    player: state.player
+  });
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  setProgress: (currentTime, duration) => dispatch(setProgress(currentTime, duration)),
+  next: () => dispatch(next()),
+  previous: () => dispatch(previous()),
+  toggle: (song, list) => dispatch(toggleSong(song, list)),
+});
+
+
 class PlayerContainer extends Component {
 
-  constructor() {
-    super();
-    this.state = store.getState().player;
+  constructor(props){
+    super(props);
     this.toggle = this.toggle.bind(this);
   }
-
   componentDidMount() {
-
-    AUDIO.addEventListener('ended', this.next);
+    AUDIO.addEventListener('ended', this.props.next);
     AUDIO.addEventListener('timeupdate', () => {
-      store.dispatch(setProgress(AUDIO.currentTime / AUDIO.duration));
+      this.props.setProgress(AUDIO.currentTime / AUDIO.duration);
+      //store.dispatch(setProgress(AUDIO.currentTime / AUDIO.duration));
     });
-
-    this.unsubscribe = store.subscribe(() => {
-      this.setState(store.getState().player);
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  next() {
-    store.dispatch(next());
-  }
-
-  prev() {
-    store.dispatch(previous());
   }
 
   toggle() {
-    store.dispatch(
-      toggleSong(this.state.currentSong, this.state.currentSongList)
-    );
+    this.props.toggle(this.props.player.currentSong, this.props.player.currentSongList);
   }
 
   render() {
     return <Player
-      {...this.state}
-      next={this.next}
-      prev={this.prev}
-      toggle={this.toggle}
+      {...this.props.player}
+      next={this.props.next}
+      prev={this.props.previous}
+      toggle={this.props.toggle}
     />;
   }
 
 }
 
-export default PlayerContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(PlayerContainer);
